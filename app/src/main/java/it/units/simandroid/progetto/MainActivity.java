db827754b55bca6c17ff9eb1e3127dd109df1328
Ivan Pelizon <24814264+peiva-git@users.noreply.github.com>
2022-09-16 16:33:34 +0200

    First commit

---
+++ app/src/main/java/it/units/simandroid/progetto/MainActivity.java
@@ -0,0 +1,14 @@
+package it.units.simandroid.progetto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
