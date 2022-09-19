2021664b8d3da38c29da3a84e076a0947ae1e2f0
Ivan Pelizon <24814264+peiva-git@users.noreply.github.com>
2022-09-19 17:56:56 +0200

    Added firebase code from tutorial page

--- app/src/main/java/it/units/simandroid/progetto/LoginActivity.java
+++ app/src/main/java/it/units/simandroid/progetto/LoginActivity.java
@@ -4,11 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class LoginActivity extends AppCompatActivity {
+
+    private FirebaseAuth firebaseInstance;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        firebaseInstance = FirebaseAuth.getInstance();
         setContentView(R.layout.activity_login);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = firebaseInstance.getCurrentUser();
+        if (currentUser != null) {
+            // reload
+        }
     }
 }
