128c88c5e77e0677cea228926f3b1f389c0f9bba
Ivan Pelizon <24814264+peiva-git@users.noreply.github.com>
2022-09-21 17:02:54 +0200

    Able to go from one activity to another Fragment can only be used as part of an activity

--- app/src/main/java/it/units/simandroid/progetto/LoginActivity.java
+++ app/src/main/java/it/units/simandroid/progetto/LoginActivity.java
@@ -2,7 +2,10 @@ package it.units.simandroid.progetto;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -10,12 +13,26 @@ import com.google.firebase.auth.FirebaseUser;
 public class LoginActivity extends AppCompatActivity {
 
     private FirebaseAuth firebaseInstance;
+    private Button loginButton;
+    private Button registrationButton;
+    private EditText userEmail;
+    private EditText userPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         firebaseInstance = FirebaseAuth.getInstance();
         setContentView(R.layout.activity_login);
+
+        loginButton = (Button) findViewById(R.id.loginButton);
+        registrationButton = (Button) findViewById(R.id.signUpButton);
+        userEmail = (EditText) findViewById(R.id.loginUsernameText);
+        userPassword = (EditText) findViewById(R.id.loginPasswordText);
+
+        registrationButton.setOnClickListener(view -> {
+            Intent goToRegistrationForm = new Intent(this, RegistrationActivity.class);
+            startActivity(goToRegistrationForm);
+        });
     }
 
     @Override
---
+++ app/src/main/java/it/units/simandroid/progetto/RegistrationActivity.java
@@ -0,0 +1,14 @@
+package it.units.simandroid.progetto;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RegistrationActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+    }
+}
