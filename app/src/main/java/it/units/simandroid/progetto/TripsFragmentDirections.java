a90ba7874af2fcdbce3320092f000ed9620ab005
Ivan Pelizon <24814264+peiva-git@users.noreply.github.com>
2022-10-10 18:41:29 +0200

    Toolbar now working on dark themes as well

--- app/src/main/java/it/units/simandroid/progetto/TripsFragmentDirections.java
+++ app/src/main/java/it/units/simandroid/progetto/TripsFragmentDirections.java
@@ -1,9 +1,14 @@
 package it.units.simandroid.progetto;
 
+import androidx.annotation.NonNull;
 import androidx.navigation.ActionOnlyNavDirections;
 import androidx.navigation.NavDirections;
 
+import org.jetbrains.annotations.Contract;
+
 public class TripsFragmentDirections {
+    @NonNull
+    @Contract(" -> new")
     public static NavDirections actionTripsFragmentToNewTripFragment() {
         return new ActionOnlyNavDirections(R.id.action_tripsFragment_to_newTripFragment);
     }
