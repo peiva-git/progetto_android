194ae21842ab18687e81439bc733f4a6b429685c
Ivan Pelizon <24814264+peiva-git@users.noreply.github.com>
2022-10-11 22:16:58 +0200

    Now using the Navigation Component, refactored whole project to implement proper usage

---
+++ app/src/main/java/it/units/simandroid/progetto/LoginFragment.java
@@ -0,0 +1,74 @@
+package it.units.simandroid.progetto;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class LoginFragment extends Fragment {
+
+    public static final String AUTH_TAG = "AUTH";
+
+    private FirebaseAuth firebaseInstance;
+    private Button loginButton;
+    private Button registrationButton;
+    private EditText userEmail;
+    private EditText userPassword;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        firebaseInstance = FirebaseAuth.getInstance();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_login, container, false);
+
+        loginButton = view.findViewById(R.id.login_button);
+        registrationButton = view.findViewById(R.id.sign_up_button);
+        userEmail = view.findViewById(R.id.login_username_text);
+        userPassword = view.findViewById(R.id.login_password_text);
+
+        registrationButton.setOnClickListener(registrationButtonView -> {
+            NavHostFragment.findNavController(this)
+                    .navigate(LoginFragmentDirections.actionLoginFragmentToRegistrationFragment());
+        });
+        loginButton.setOnClickListener(loginButtonView -> {
+            firebaseInstance.signInWithEmailAndPassword(userEmail.getText().toString(), userPassword.getText().toString())
+                    .addOnCompleteListener(task -> {
+                        if (task.isSuccessful()) {
+                            Log.d(AUTH_TAG, "Sign-in successful");
+                        } else {
+                            Log.w(AUTH_TAG, "Sign-in failed", task.getException());
+                            Toast.makeText(getContext(), "Authentication failed", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+        });
+
+        return view;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = firebaseInstance.getCurrentUser();
+        if (currentUser != null) {
+            NavHostFragment.findNavController(this)
+                    .navigate(LoginFragmentDirections.actionLoginFragmentToTripsFragment());
+        }
+    }
+}
--- app/src/main/java/it/units/simandroid/progetto/RegistrationFragment.java
+++ app/src/main/java/it/units/simandroid/progetto/RegistrationFragment.java
@@ -1,20 +1,25 @@
 package it.units.simandroid.progetto;
 
-import static it.units.simandroid.progetto.LoginActivity.AUTH_TAG;
+import static it.units.simandroid.progetto.LoginFragment.AUTH_TAG;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
 
 import com.google.firebase.auth.FirebaseAuth;
 
-public class RegistrationActivity extends AppCompatActivity {
+public class RegistrationFragment extends Fragment {
 
     private EditText userEmail;
     private EditText userEmailConfirm;
@@ -26,33 +31,42 @@ public class RegistrationActivity extends AppCompatActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_registration);
 
-        userEmail = findViewById(R.id.registration_email);
-        userEmailConfirm = findViewById(R.id.registration_email_confirm);
-        userPassword = findViewById(R.id.registration_password);
-        userPasswordConfirm = findViewById(R.id.registration_password_confirm);
-        registrationButton = findViewById(R.id.registration_button);
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_registration, container, false);
+
+        userEmail = view.findViewById(R.id.registration_email);
+        userEmailConfirm = view.findViewById(R.id.registration_email_confirm);
+        userPassword = view.findViewById(R.id.registration_password);
+        userPasswordConfirm = view.findViewById(R.id.registration_password_confirm);
+        registrationButton = view.findViewById(R.id.registration_button);
 
         firebaseInstance = FirebaseAuth.getInstance();
 
-        registrationButton.setOnClickListener(view -> {
+        registrationButton.setOnClickListener(registrationButtonView -> {
             if (!validateForm()) {
                 return;
             }
             firebaseInstance.createUserWithEmailAndPassword(userEmail.getText().toString(), userPassword.getText().toString())
-                    .addOnCompleteListener(this, task -> {
+                    .addOnCompleteListener(task -> {
                         if (task.isSuccessful()) {
                             Log.d(AUTH_TAG, "User created successfully");
-                            Intent goToMainActivity = new Intent(RegistrationActivity.this, MainActivity.class);
-                            startActivity(goToMainActivity);
+                            NavHostFragment.findNavController(this)
+                                    .navigate(RegistrationFragmentDirections.actionRegistrationFragmentToTripsFragment());
                         } else {
                             Log.w(AUTH_TAG, "Failed to create new user", task.getException());
-                            Toast.makeText(RegistrationActivity.this, "Authentication failed", Toast.LENGTH_SHORT).show();
+                            Toast.makeText(getContext(), "Authentication failed", Toast.LENGTH_SHORT).show();
                             // updateUI(null)
                         }
                     });
         });
+
+        return view;
     }
 
     private boolean validateForm() {
