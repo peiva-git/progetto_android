012b16b0b6ecb31000175c8187b2e03933f9a933
Ivan Pelizon <24814264+peiva-git@users.noreply.github.com>
2022-11-18 20:24:59 +0100

    Syncing settings with remote database

--- app/src/main/java/it/units/simandroid/progetto/MainActivity.java
+++ app/src/main/java/it/units/simandroid/progetto/MainActivity.java
@@ -1,5 +1,8 @@
 package it.units.simandroid.progetto;
 
+import static it.units.simandroid.progetto.RealtimeDatabase.DB_URL;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
@@ -7,7 +10,9 @@ import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+import androidx.preference.PreferenceManager;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -18,8 +23,17 @@ import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.progressindicator.LinearProgressIndicator;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.GenericTypeIndicator;
+import com.google.firebase.database.ValueEventListener;
 
 import org.jetbrains.annotations.NotNull;
+
+import java.util.Map;
+import java.util.Objects;
+import java.util.concurrent.atomic.AtomicMarkableReference;
 
 import it.units.simandroid.progetto.fragments.directions.TripsFragmentArgs;
 import it.units.simandroid.progetto.fragments.directions.TripsFragmentDirections;
@@ -32,11 +46,17 @@ public class MainActivity extends AppCompatActivity {
     private DrawerLayout navigationDrawer;
     private NavigationView navigationView;
     private LinearProgressIndicator progressIndicator;
+    private FirebaseAuth authentication;
+    private FirebaseDatabase database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        authentication = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance(DB_URL);
+
         toolbar = findViewById(R.id.toolbar);
         navigationDrawer = findViewById(R.id.drawer_layout);
         navigationView = findViewById(R.id.navigation_view);
@@ -96,8 +116,52 @@ public class MainActivity extends AppCompatActivity {
                 navigationDrawer.close();
                 navController.navigate(action);
                 return true;
+            } else if (item.getItemId() == R.id.settings) {
+                navigationDrawer.close();
+                navController.navigate(R.id.action_global_settings);
+                return true;
             }
             return false;
+        });
+
+        database.getReference("users").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (authentication.getUid() != null) {
+                    String userName = snapshot
+                            .child(authentication.getUid())
+                            .child("name")
+                            .getValue(String.class);
+                    String userSurname = snapshot
+                            .child(authentication.getUid())
+                            .child("surname")
+                            .getValue(String.class);
+
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        preferences.registerOnSharedPreferenceChangeListener((sharedPreferences, key) -> {
+            if (key.equals("user_name")) {
+                database.getReference("users")
+                        .child(Objects.requireNonNull(authentication.getUid()))
+                        .child("name")
+                        .setValue(sharedPreferences.getString(key, ""));
+                Log.i("SETTINGS", "Preference value was updated to: " + sharedPreferences.getString(key, ""));
+            } else if (key.equals("user_surname")) {
+                database.getReference("users")
+                        .child(Objects.requireNonNull(authentication.getUid()))
+                        .child("surname")
+                        .setValue(sharedPreferences.getString(key, ""));
+                Log.i("SETTINGS", "Preference value was updated to: " + sharedPreferences.getString(key, ""));
+            }
         });
     }
 
--- app/src/main/java/it/units/simandroid/progetto/fragments/SettingsFragment.java
+++ app/src/main/java/it/units/simandroid/progetto/fragments/SettingsFragment.java
@@ -1,9 +1,21 @@
 package it.units.simandroid.progetto.fragments;
 
+import static it.units.simandroid.progetto.RealtimeDatabase.DB_URL;
+
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 
+import androidx.preference.EditTextPreference;
 import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceManager;
+
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Objects;
 
 import it.units.simandroid.progetto.R;
 
@@ -12,5 +24,21 @@ public class SettingsFragment extends PreferenceFragmentCompat {
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.settings, rootKey);
+        EditTextPreference namePreference = findPreference("user_name");
+        EditTextPreference surnamePreference = findPreference("user_surname");
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance(DB_URL);
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        Task<DataSnapshot> task = database.getReference("users")
+                .child(Objects.requireNonNull(auth.getUid()))
+                .get();
+        task.addOnCompleteListener(taskCompleted -> {
+            DataSnapshot snapshot = taskCompleted.getResult();
+            String userName = snapshot.child("name").getValue(String.class);
+            String userSurname = snapshot.child("surname").getValue(String.class);
+
+            Objects.requireNonNull(namePreference).setText(userName);
+            Objects.requireNonNull(surnamePreference).setText(userSurname);
+        });
     }
 }
