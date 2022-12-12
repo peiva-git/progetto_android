a3bfee3188e70dbfd1d6491091d8d95b96f89c36
Ivan Pelizon <24814264+peiva-git@users.noreply.github.com>
2022-12-12 13:56:36 +0100

    Refactor

--- app/src/main/java/it/units/simandroid/progetto/adapters/OnFavoriteStateChangedListener.java
+++ app/src/main/java/it/units/simandroid/progetto/adapters/OnFavoriteStateChangedListener.java
@@ -2,8 +2,6 @@ package it.units.simandroid.progetto.adapters;
 
 import android.widget.CompoundButton;
 
-import it.units.simandroid.progetto.Trip;
-
 public interface OnFavoriteStateChangedListener {
     void onFavoriteStateChanged(int position, CompoundButton compoundButton, boolean isChecked);
 }
--- app/src/main/java/it/units/simandroid/progetto/adapters/OnTripClickListener.java
+++ app/src/main/java/it/units/simandroid/progetto/adapters/OnTripClickListener.java
@@ -1,10 +1,5 @@
 package it.units.simandroid.progetto.adapters;
 
-import android.view.View;
-import android.widget.CompoundButton;
-
-import it.units.simandroid.progetto.Trip;
-
 public interface OnTripClickListener {
     void onTripClick(int position);
     boolean onTripLongClick(int position);
