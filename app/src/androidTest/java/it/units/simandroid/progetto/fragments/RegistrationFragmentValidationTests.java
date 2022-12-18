503ac1552fb03a3f05f2961fbd9a99448a828b6a
Ivan Pelizon <24814264+peiva-git@users.noreply.github.com>
2022-12-18 13:41:51 +0100

    Added tests to check email and password mismatch errors

--- app/src/androidTest/java/it/units/simandroid/progetto/fragments/RegistrationFragmentValidationTests.java
+++ app/src/androidTest/java/it/units/simandroid/progetto/fragments/RegistrationFragmentValidationTests.java
@@ -20,14 +20,23 @@ import it.units.simandroid.progetto.R;
 @LargeTest
 public class RegistrationFragmentValidationTests {
 
+    public static final String CORRECT_FORMAT_EMAIL = "test@units.it";
+    public static final String WRONG_FORMAT_EMAIL = "test";
+    public static final String SHORT_PASSWORD = "pass";
+    public static final String CORRECT_PASSWORD = "password";
+
     private FragmentScenario<RegistrationFragment> scenario;
     private String fieldRequiredError;
+    private String emailsMismatchError;
+    private String passwordsMismatchError;
 
     @Before
     public void init() {
         scenario = FragmentScenario.launchInContainer(RegistrationFragment.class, Bundle.EMPTY, R.style.Theme_ProgettoSIMAndroid);
         scenario.onFragment(registrationFragment -> {
             fieldRequiredError = registrationFragment.getString(R.string.field_required);
+            emailsMismatchError = registrationFragment.getString(R.string.email_mismatch);
+            passwordsMismatchError = registrationFragment.getString(R.string.password_mismatch);
         });
     }
 
@@ -43,6 +52,32 @@ public class RegistrationFragmentValidationTests {
                 .check(ViewAssertions.matches(hasTextInputLayoutErrorText(fieldRequiredError)));
         Espresso.onView(ViewMatchers.withId(R.id.registration_password_layout))
                 .check(ViewAssertions.matches(hasTextInputLayoutErrorText(fieldRequiredError)));
+    }
+
+    @Test
+    public void checkErrorsWhenEmailsAreDifferent() {
+        Espresso.onView(ViewMatchers.withId(R.id.registration_email))
+                .perform(ViewActions.typeText(CORRECT_FORMAT_EMAIL))
+                .perform(ViewActions.closeSoftKeyboard());
+        Espresso.onView(ViewMatchers.withId(R.id.registration_button))
+                .perform(ViewActions.click());
+        Espresso.onView(ViewMatchers.withId(R.id.registration_email_layout))
+                .check(ViewAssertions.matches(hasTextInputLayoutErrorText(emailsMismatchError)));
+        Espresso.onView(ViewMatchers.withId(R.id.registration_email_confirm_layout))
+                .check(ViewAssertions.matches(hasTextInputLayoutErrorText(emailsMismatchError)));
+    }
+
+    @Test
+    public void checkErrorsWhenPasswordsAreDifferent() {
+        Espresso.onView(ViewMatchers.withId(R.id.registration_password))
+                .perform(ViewActions.typeText(CORRECT_PASSWORD))
+                .perform(ViewActions.closeSoftKeyboard());
+        Espresso.onView(ViewMatchers.withId(R.id.registration_button))
+                .perform(ViewActions.click());
+        Espresso.onView(ViewMatchers.withId(R.id.registration_password_layout))
+                .check(ViewAssertions.matches(hasTextInputLayoutErrorText(passwordsMismatchError)));
+        Espresso.onView(ViewMatchers.withId(R.id.registration_password_confirm_layout))
+                .check(ViewAssertions.matches(hasTextInputLayoutErrorText(passwordsMismatchError)));
     }
 
     @After
